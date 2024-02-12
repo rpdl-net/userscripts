5f06495e35cad1cc8603b8198540fb03eaecbcc6
devilkin <devilkin@noreply.git.rpdl.net>
2024-02-12 17:17:24 +0000

    Update userscript.js comments

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RPDL Enhancement Userscript
 // @version      Alpha
-// @description  Userscript providing enhancements for RPDL uploader (dl.rpdl.net, jenkins, F95Zone.)
+// @description  Userscript providing enhancements for uploaders (dl.rpdl.net, Jenkins, F95Zone).
 // @author       RPDL Team
 // @match        https://dl.rpdl.net/*
 // @match        https://jenkins.rpdl.net/*
@@ -18,29 +18,31 @@
 (function() {
     'use strict';
 
-    // Define your username here. Used for build-${username}-new and dropdown selectors (transfer, token-update)
+    // Define your Jenkins username here; used for build-new and dropdown selectors (transfer, token-update)
     const username = "{your-username}";
 
-    // Function pulls Id from URL
+    // Function pulls Id from torrent page URL
     function getId() {
         var url = window.location.href;
         var match = url.match(/\/(\d+)$/);
         if (match) {
             var id = match[1];
+            // Saves it to GM_setValue('torrentid')
             GM_setValue('torrentid', id);
         }
     }
 
-    // Function pulls name from page heading
+    // Function pulls name from torrent page
     function getName() {
         const h1Element = document.querySelector('h1.py-2.text-xl.font-semibold.text-slate-200.truncate');
         if (h1Element) {
             const releasename = h1Element.textContent.trim();
+            // Saves it to GM_setValue('releasename')
             GM_setValue('releasename', releasename);
         }
     }
 
-    // Function pulls funding link from markdown description
+    // Function pulls funding link from torrent page markdown description
     function getFunding() {
         const markdownBody = document.querySelector('div.markdown-body');
         if (markdownBody) {
@@ -48,19 +50,20 @@
             for (const linkElement of fundingLinkElements) {
                 if (linkElement.textContent.trim() === 'Developer Funding') {
                     const fundinglink = linkElement.getAttribute('href');
+                    // Saves it to GM_setValue('funding')
                     GM_setValue('funding', fundinglink);
                 }
             }
         }
     }
 
-    // Call the three functions to retrieve and store values
+    // Function calls the (3)functions that pull values and saves to GM_setValue(s)
     function getAll() {
         getId();
         getName();
         getFunding();
     }
-    // Fill input field using the name as both the GM value name and element value. overrideValue is optional
+    // Function fills input fields on Jenkins as both GM values and Element values. overrideValue is optional
     function fillInputField(name, overrideValue){
         const value = overrideValue ?? GM_getValue(name);
         const inputField = document.querySelector('input[name="name"][type="hidden"][value="' + name + '"]');
@@ -68,17 +71,26 @@
             const nextInput = inputField.nextElementSibling;
             if(nextInput && nextInput.tagName.toLowerCase() == "input"){
                 nextInput.value = value || "";
+                // Replaces "undefined" values as blank, so as to not fill boxes when no data was pulled
+                // Can be replaced with next.Input.value = value; to debug and check if script pastes values in the right area
+                // If values are being pasted in the right area, but still come back as undefined/blank, it means that the clearAllValues function is being called too early
             }
         }
     }
-    // Function which pastes the pulled information if one of the available boxes is present
+    // Function pastes the pulled values to their respective boxes, if found
     function pasteAll() {
+        // Fills the "torrentid" box found on Torrent-Delete, Torrent-Rename, and Torrent-Transfer
         fillInputField("torrentid");
+        // Fills the "releasename" box found on Build-New
         fillInputField("releasename");
+        // Fills the "funding" box found on Build-New
         fillInputField("funding");
 
+        // Fills the "newname" box found on Torrent-Rename using the "releasename" value
         fillInputField("newname", GM_getValue('releasename'));
 
+        // Selects and fills the dropdown found on Torrent-Transfer with the username defined at the beginning of the script
+        // Paste is set to occur on page load, so dropdown can still be modified if transfering to another uploader
         const dropdown = document.querySelector('div.jenkins-select[name="parameter"]');
         if (dropdown) {
             const option = dropdown.querySelector(`option[value="${username}"]`);
@@ -86,10 +98,11 @@
                 option.selected = true;
             }
         }
+        // Calls clearAllValues to remove saved/pasted values from storage
         clearAllValues();
     }
     
-    // Function to create a button & add click handlers for left-click, middle-click, or new tab opening
+    // Function creates button appearance, and adds click handlers for left-click, middle-click, or new tab opening
     function createButton(text, url) {
         const button = document.createElement('button');
         button.href = url;
@@ -108,9 +121,9 @@
         });
         return button;
     }
-    // Function to add buttons on torrent pages
+
+    // Function adds the buttons on torrent pages
     function addButtonsOnTorrentPage() {
-        // Check if the page is a torrent page on dl.rpdl.net with the specified pattern
         const torrentPage = document.querySelector('div .truncate').parentElement;
         const buildNewButton = createButton('Build-new', `https://jenkins.rpdl.net/job/build-${username}-new/build`);
         const deleteButton = createButton('Delete', 'https://jenkins.rpdl.net/job/torrent-delete/build');
@@ -126,25 +139,35 @@
         
         torrentPage.appendChild(buttonContainer);
     }
+
     function init(){
+        // Checks if the current page is a Jenkins job or a torrent page
         const isJenkinsJob = window.location.href.startsWith("https://jenkins.rpdl.net/job/");
         const isTorrentPage = window.location.href.match(/^https:\/\/dl\.rpdl\.net\/torrent\/\d+$/);
         
+        // If on a Jenkins job, it finds the elements needed to identify what values need to be pasted, and it calls pasteAll
         if(isJenkinsJob){
             waitForKeyElements('input[name="name"][type="hidden"][value="torrentid"], input[name="name"][type="hidden"][value="releasename"], input[name="name"][type="hidden"][value="funding"]', pasteAll);
+        // If on a torrent page, it adds the buttons
         }else if(isTorrentPage){
             waitForKeyElements("div .truncate", addButtonsOnTorrentPage)
+        // If on another page, calls clearAllValues to remove saved/pasted values from storage
         }else{
             clearAllValues();
         }
     }
+
+    // When changing pages, calls init to check if on Jenkins job or torrent page
     if(!window.onurlchange){
         window.addEventListener("urlchange", init);
     }
+
+    // Function clears all values from GM_setValue storage once the pasting of values, and selecting of dropdown has occured
     function clearAllValues() {
         GM_deleteValue('torrentid');
         GM_deleteValue('releasename');
         GM_deleteValue('funding');
     }
+    // Calls init to check if on Jenkins job or torrent page
     init();
 })();
