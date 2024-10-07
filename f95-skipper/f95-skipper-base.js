2d6cefd9b8b2fa37160fceff30ce224df93c064f
sake-bottle <183997837+sake-bottle@users.noreply.github.com>
2024-10-07 21:44:11 +0000

    Update f95-skipper-base.js

--- f95-skipper/f95-skipper-base.js
+++ f95-skipper/f95-skipper-base.js
@@ -7,7 +7,7 @@
 // @author       sake-bottle (forked from Cat-Ling)
 // @match        https://f95zone.to/masked/*
 // @exclude      https://f95zone.to/masked/
-/// @grant       none
+// @grant       none
 // @license      GPL-2.0
 // @supportURL   https://github.com/rpdl-net/userscripts/issues
 // @updateURL    https://raw.githubusercontent.com/rpdl-net/userscripts/refs/heads/main/f95-skipper/f95-skipper-base.js
