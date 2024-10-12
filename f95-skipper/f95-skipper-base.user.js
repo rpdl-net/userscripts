8907dcfe142e3e20f930282a361a0d1ea36f27a4
sake-bottle <183997837+sake-bottle@users.noreply.github.com>
2024-10-12 01:27:00 +0000

    Fixes metadata issues.

--- f95-skipper/f95-skipper-base.user.js
+++ f95-skipper/f95-skipper-base.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name         F95 Skipper Base
-// @icon         https://f95zone.to/favicon.ico
-// @homepageURL  https://github.com/rpdl-net/userscripts/tree/main/f95-skipper
-// @version      1.2.3
-// @description  Script automating skipping the "Link Masked" page when following select F95 file hosts' links.
-// @author       sake-bottle (forked from Cat-Ling)
-// @match        https://f95zone.to/masked/*
-// @exclude      https://f95zone.to/masked/
-// @grant        none
-// @license      GPL-2.0
-// @supportURL   https://github.com/rpdl-net/userscripts/issues
-// @updateURL    https://raw.githubusercontent.com/rpdl-net/userscripts/refs/heads/main/f95-skipper/f95-skipper-base.js
-// @downloadURL  https://raw.githubusercontent.com/rpdl-net/userscripts/refs/heads/main/f95-skipper/f95-skipper-base.js
+// @name 			F95 Skipper Base
+// @namespace		https://github.com/rpdl-net/userscripts/
+// @version			1.2.3
+// @description 	Auto-clicks the "Continue to.." button on the "Link Masked" page for F95 file host redirects.
+// @author 			Cat-Ling (forked by sake-bottle)
+// @match 			https://f95zone.to/masked/*
+// @exclude			https://f95zone.to/masked/
+// @icon			https://f95zone.to/favicon.ico
+// @homepage		https://github.com/rpdl-net/userscripts/tree/main/f95-skipper
+// @updateURL		https://raw.githubusercontent.com/rpdl-net/userscripts/refs/heads/main/f95-skipper/f95-skipper-base.user.js
+// @downloadURL		https://raw.githubusercontent.com/rpdl-net/userscripts/refs/heads/main/f95-skipper/f95-skipper-base.user.js
+// @supportURL		https://github.com/rpdl-net/userscripts/issues
 // ==/UserScript==
 
 (function() {
